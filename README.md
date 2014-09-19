0af7a829927e3eb58ab99ca3ac48abeb92fa2a7a
Carl Henderson <chuckhendo@gmail.com>
2014-09-19 00:25:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# uxgen
+
+Run this and get a fully functioning site with SASS, Coffeescript, Autoprefixer, and BrowserSync.
+
+## Requirements
+
+- Node (http://nodejs.org)
+- Yeoman (http://yeoman.io)
+- Gulp (http://gulpjs.com)
+
+
---
+++ app/index.js
@@ -0,0 +1,36 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var UxgenGenerator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+    done();
+  },
+
+  writing: {
+    app: function () {
+      this.dest.mkdir('components');
+
+      this.src.copy('_package.json', 'package.json');
+      this.src.copy('_bower.json', 'bower.json');
+      this.src.copy('_Gulpfile.js', 'Gulpfile.js');
+      this.src.copy('index.html', 'index.html');
+      this.src.copy('css/app.scss', 'css/app.scss');
+      this.src.copy('js/app.coffee', 'js/app.coffee');
+      this.src.copy('partials/partial.html', 'partials/partial.html');
+    }
+  },
+
+  end: function () {
+    this.installDependencies();
+  }
+});
+
+module.exports = UxgenGenerator;
