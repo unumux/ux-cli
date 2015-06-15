daace003b03211c0ff24d50f779b212abc397131
Carl Henderson <chuckhendo@gmail.com>
2015-06-15 10:33:57 -0400

    Added and updated automated setup scripts

---
+++ setup/osx.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+setGitToHttps() {
+  echo "Setting git to use https instead of ssh"
+  git config --global url."https://".insteadOf git://
+}
+
+checkCmd() {
+  echo "Checking if $1 is installed..."
+  if hash $1 2>/dev/null; then
+    echo "$1 is already installed"
+    return 0
+  else
+    echo "$1 is not installed"
+    return 1
+  fi
+}
+
+installNvm() {
+  echo "Installing nvm..."
+  touch ~/.profile
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
+  source ~/.profile
+  echo "Nvm installed!"
+}
+
+installNode() {
+  echo "Installing node..."
+  nvm install v0.12
+  nvm alias default 0.12
+  nvm use default
+  echo "Node installed!"
+}
+
+installNpm() {
+  echo "Installing $1..."
+  npm install $1 -g
+  echo "$1 installed!"
+}
+
+clear
+echo "Preparing to install ux-cli and it's dependencies..."
+
+if ! xcode-select -p 2>/dev/null ; then
+  echo "You do not have command line developer tools installed."
+  echo "Next, a window is going to open asking to install these tools."
+  echo "Press install, and do not continue on until they have finished installing"
+  echo "Press any key to install command line developer tools..."
+  read
+  echo "Installing command line developer tools..."
+  xcode-select --install
+  echo "Press any key once the installation has finished..."
+  read
+fi
+
+
+if ! checkCmd node ; then
+  if ! checkCmd nvm; then
+    installNvm
+  fi
+  installNode
+fi
+
+if ! checkCmd npm ; then
+  echo "Something is wrong with your installation of NodeJS. Please uninstall and run this script again"
+  exit 1;
+fi
+
+setGitToHttps
+
+
+if ! checkCmd bower ; then
+  installNpm bower
+fi
+
+installNpm @unumux/ux
+
+echo "Finished! You may need to close this window and open a new terminal window before you can use the tool"
---
+++ setup/win.ps1
@@ -0,0 +1,103 @@
+$tmp = $env:TEMP
+
+Function Reload-Path {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+}
+
+Function Download-File($url, $targetFile, $auth) {
+   $uri = New-Object "System.Uri" "$url"
+   $request = [System.Net.HttpWebRequest]::Create($uri)
+   if($auth) {
+     $request.Headers.Add('authorization', 'Basic N2M0MmNlNzNmODg1MzgyNmNkMWUzYTVkNDcwMDJjN2ZmNjkzMTY0Zjp4LW9hdXRoLWJhc2ljCg==')
+   }
+   $request.set_Timeout(15000) #15 second timeout
+   $response = $request.GetResponse()
+   $responseContentLength = $response.get_ContentLength()
+
+   if([int]$response.StatusCode -eq 200) {
+     if(-not ($responseContentLength -lt 1024) ) {
+       $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+     } else {
+       $totalLength = [System.Math]::Floor(1024/1024)
+     }
+     $responseStream = $response.GetResponseStream()
+     $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+     $buffer = new-object byte[] 10KB
+     $count = $responseStream.Read($buffer,0,$buffer.length)
+     $downloadedBytes = $count
+     while ($count -gt 0)
+     {
+         $targetStream.Write($buffer, 0, $count)
+         $count = $responseStream.Read($buffer,0,$buffer.length)
+         $downloadedBytes = $downloadedBytes + $count
+         Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+     }
+     Write-Progress -completed -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete 100
+     $targetStream.Flush()
+     $targetStream.Close()
+     $targetStream.Dispose()
+     $responseStream.Dispose()
+   }
+}
+
+Function Download-And-Install($name, $url, $file) {
+  echo "$name not installed. Downloading $name..."
+  Download-File $url $file
+  echo "Installing $name..."
+  $installStatement = [System.Diagnostics.Process]::Start( "msiexec", "/i $file /quiet" )
+  $installStatement.WaitForExit()
+  Reload-Path
+}
+
+Function Run-Main {
+  echo "Checking for dependencies..."
+
+
+  if(-not (Get-Command node 2>$null)) {
+    Download-And-Install "node" "http://nodejs.org/dist/v0.12.4/node-v0.12.4-x86.msi" "$tmp\node.msi"
+  }
+
+  git config --global url."https://".insteadOf git://
+
+  npm install -g @unumux/ux-cli bower
+}
+
+
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   # We are running "as Administrator" - so change the title and background color to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+   clear-host
+   }
+else
+   {
+   # We are not running "as Administrator" - so relaunch as administrator
+
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+
+   # Exit from the current, unelevated, process
+   exit
+   }
+
+Run-Main
+
+Write-Host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
