db0e249b34485eb2a76c5dc08b8e7f655784527b
Carl Henderson <chuckhendo@gmail.com>
2015-09-09 11:01:06 -0400

    2.12.2

--- app/index.js
+++ app/index.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
-require('babel/register')({
-    stage: 1
+var path = require('path');
+
+// path to node_modules
+var nmPath = path.join(__dirname, '../', 'node_modules');
+
+var babel = require('babel/register')({
+    stage: 1,
+    ignore: nmPath // set to not compile anything inside node_modules
 });
 require('./main.js')();
